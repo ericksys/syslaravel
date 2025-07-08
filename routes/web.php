0680d29a44de74afa502ac99061a20213f324a3a
ericksys <erickzkds@gmail.com>
2025-07-07 22:20:26 -0300

    att das aulas 1 a 4

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = 'Eric';
+    return view('welcome', ['nome' => $nome]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produto', function () {
+    return view('products');
 });
